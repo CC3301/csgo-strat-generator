170d50ccb9cf537fa6a7aad750698a4e965891d8
fink <flo.fink.124@web.de>
2019-09-18 14:32:31 +0000

    Cleaned up csgo.pl, added command line arguments

--- csgo.pl
+++ csgo.pl
@@ -2,165 +2,92 @@
 use strict;
 use warnings;
 
-use lib 'lib';
-use Feature::Items;
-use Feature::Strats;
-use Feature::Hardcore;
+use lib 'lib/';
 use Util::Debug;
-use Util::ReadInventory;
+use Util::Parser;
+use Util::Help;
+use Main;
 
 # debug for this main part of csgo-strat gen on or off
-my $DEBUG_STATE = 0;
+my $DEBUG_STATE = 1;
 my $VERSION = 'v1.0';
 
 ################################################################################
 # Main subroutine
 ################################################################################
 sub Main() {
+
+  _local_debug("[WRAP] : Kicking off csgo-strat-generator $VERSION");
+
+  #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  # parse command line options
+  #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  my %state = Util::Parser::Parse(@ARGV);
+
+  #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  # other vars 
+  #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  my $difficulty = $state{difficulty};
   
-  # initialize the debug output
-  _local_debug("[MAIN] : Starting csgo strat gen..");
-
   #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  # get difficulty
+  # check if help or rules are required 
   #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  my $difficulty = $ARGV[0] || 0;
-
-  unless($difficulty =~ /^-?\d+$/) {
-    if ($difficulty eq 'rules' || $difficulty eq 'r') {
-      _local_debug("[MAIN] : Showing rules");
-      _show_rules();
-      return(0);
-    } elsif($difficulty eq 'help' || $difficulty eq 'h') {
-      _local_debug("[MAIN] : Showing help");
-      _show_help();
-      return(0);
-    }
-    die "Invalid argument, use h or help for further information";
+  if ($state{help}) {
+    Util::Help::ShowMsg('help');
+    _local_debug("[MAIN] : Early exit. Help was displayed.");
+    exit();
   }
-
-  _local_debug("[MAIN] : Using difficulty: $difficulty");
-
-  #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  # generate weapons 
-  #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  
-  # generate pistol and check for free choice
-  my %pistol = _generate_weapon_set('pistols');
-  while(_check_for_free_choice($difficulty, %pistol)) {
-    %pistol = _generate_weapon_set('pistols');
-  }
-  
-  # generate weapon and check for free choice
-  my %weapon = _generate_weapon_set('weapons');
-  while(_check_for_free_choice($difficulty, %weapon)) {
-    %weapon = _generate_weapon_set('weapons');
-  }
-
-  # generate grenade1 and check for free choice
-  my %grenade1 = _generate_weapon_set('grenades');
-  while(_check_for_free_choice($difficulty, %grenade1)) {
-    %grenade1 = _generate_weapon_set('grenades');
-  }
-
-  # generate grenade2 and check for free choice
-  my %grenade2 = _generate_weapon_set('grenades');
-  while(_check_for_free_choice($difficulty, %grenade2) || _check_for_duplicate(\%grenade1, \%grenade2)) {
-    %grenade2 = _generate_weapon_set('grenades');
-  }
-
-  # generate util2 and check for free choice
-  my %util1 = _generate_weapon_set('utils');
-  while(_check_for_free_choice($difficulty, %util1)) {
-    %util1 = _generate_weapon_set('utils');
-  }
-
-  # generate util2 and check for free choice
-  my %util2 = _generate_weapon_set('utils');
-  while(_check_for_free_choice($difficulty, %util2) || _check_for_duplicate(\%util1, \%util2)) {
-    %util2 = _generate_weapon_set('utils');
-  }
-
-  # prevent kevlar and kevlar+helmet from being there together and remove duplicates
-  if ($util1{buy} eq 'vesthelm') {
-    while($util2{buy} eq 'vest') {
-      %util2 = _generate_weapon_set('utils');
-      if (_check_for_free_choice($difficulty, %util2) || _check_for_duplicate(\%util1, \%util2)) {
-        $util2{buy} = 'vest';
-      }
-    }
-  }
-  if ($util2{buy} eq 'vesthelm') {
-    while($util1{buy} eq 'vest') {
-      %util1 = _generate_weapon_set('utils');
-      if (_check_for_free_choice($difficulty, %util2) || _check_for_duplicate(\%util1, \%util2)) {
-        $util1{buy} = 'vest';
-      }
-    }
-  }
-
-  # generate strat
-  my %strat = Feature::Strats::GetStrat($difficulty);
-
-  # generate hardcore settings
-  my @hardcore_settings = Feature::Hardcore::GetHardcore($difficulty);
-
-  #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  # add all the costs together
-  #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  my $total_cost = 0;
-  if ($difficulty >= 0) {
-    $total_cost = $pistol{cost} + $weapon{cost};
-  }
-  if ($difficulty >= 1) {
-    $total_cost = $total_cost + $grenade1{cost} + $grenade2{cost};
-  }
-  if ($difficulty >= 2) {
-    $total_cost = $total_cost + $util1{cost} + $util2{cost};
+  if ($state{rules}) {
+    Util::Help::ShowMsg('rules');
+    _local_debug("[MAIN] : Early exit. Rules were displayed.");
+    exit();
   }
 
   #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  # add all the buy commands together
+  # get dataset 
   #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  my $command_string = "";
-  my @buys = ($pistol{buy}, $util1{buy}, $util2{buy},
-              $grenade1{buy}, $grenade2{buy}, $weapon{buy});
-  foreach(@buys) {
-    next if $_ eq "NONE";
-    next if $_ eq "NULL";
-    $command_string = $command_string . " buy $_;";
-  }
-   
+  my %data = Main::Run($difficulty, %state);
+
   #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  # print data
+  # print output 
   #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  _local_debug("[MAIN] : Priting output");
+  _local_debug("[WRAP] : Priting output");
   print "CSGO strat-generator $VERSION output:\n\n";
   print "===================================================================\n";
   
-  if ($difficulty >= 0) {
-    print "Pistol   : " . $pistol{name}   . "\n";
-    print "Weapon   : " . $weapon{name}   . "\n";
+  if ($difficulty >= 0 && ! $state{disable}{pistol}) {
+    print "Pistol   : " . $data{pistol_name}   . "\n";
   }
-  if ($difficulty >= 1) {
-    print "Grenades : " . $grenade1{name} . ", " . $grenade2{name} . "\n";
+  if ($difficulty >= 0 && ! $state{disable}{weapon}) {
+    print "Weapon   : " . $data{weapon_name}   . "\n";
   }
-  if ($difficulty >= 2) {
-    print "Utilities: " . $util1{name} . ", " . $util2{name} . "\n";
+  if ($difficulty >= 1 && ! $state{disable}{grenades}) {
+    print "\n";
+    print "Grenades: \n";
+    foreach(split ';', $data{grenade_names}) {
+      print "\t- $_\n";
+    }
   }
-  if ($difficulty >= 6) {
+  if ($difficulty >= 2 && ! $state{disable}{utils}) {
+    print "\n";
+    print "Utilities: \n";
+    foreach(split ';', $data{util_names}) {
+      print "\t- $_\n";
+    }
+  }
+  if ($difficulty >= 6 && ! $state{disable}{hardcore}) {
     print "\n";
     print "Hardcore settings: \n";
-    foreach(@hardcore_settings) {
+    foreach(split ';', $data{hardcore}) {
       print "\t- $_\n";
     }
     print "\nNOTE: Hardcore settings overwrite strats and generated weapon sets.\n";
   }
-  if ($difficulty >= 3) {
+  if ($difficulty >= 3 && ! $state{disable}{strats}) {
     print "\n";
-    print "Strat: " . $strat{name} . "\n";
+    print "Strat: " . $data{strat_name} . "\n";
     print "Description:\n";
-    foreach(split ';', $strat{desc}) {
+    foreach(split ';', $data{strat_desc}) {
       print "\t$_\n";
     }
   }
@@ -168,141 +95,15 @@ sub Main() {
   # print the rest of the stats
   print "===================================================================\n";
   print "\nDifficulty : $difficulty\n";
-  print "Total Cost : $total_cost\$\n\n";
-  print "Command: bind <key>\"$command_string\"\n";
+  print "Total Cost : $data{total_cost}\$\n\n";
+  print "Command: bind $state{default_key} \"$data{command_string}\"\n";
 
   #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
   # return
   #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  _local_debug("[MAIN] : csgo strat gen complete.");
-  _local_debug("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" .
-  "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n", 1);
+  _local_debug("[WRAP] : csgo strat gen complete.");
   return(0);
   
-}
-
-################################################################################
-# _generate_weapon_set subroutine
-################################################################################
-sub _generate_weapon_set {
-
-  #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  # get vars passed to the function
-  #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  my $type = shift;
-
-  #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  # other vars
-  #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  my %item;
-
-  #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  # generate datasets
-  #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  %item = Feature::Items::GetItem($type);
-
-  #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  # return data
-  #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  return(%item);
-
-}
-
-################################################################################
-# _check_for_free_choice subroutine
-################################################################################
-sub _check_for_free_choice {
-
-  #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  # get vars passed to the function
-  #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  my $difficulty = shift;
-  my %item = @_;
-  
-  #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  # return when the difficulty is too low
-  #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  return(0) if $difficulty < 3;
-
-  #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  # check each dataset for free choice
-  #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  if ($item{buy} eq "NONE") {
-    _local_debug("[MAIN] : Found free choice, running difficulty $difficulty. Regenerating");
-    return 1;
-  }
-
-  #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  # return data
-  #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  return 0;
-
-}
-
-################################################################################
-# _check_for_duplicate subroutine 
-################################################################################
-sub _check_for_duplicate {
-
-  #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  # get vars passed to the function
-  #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  my $item1 = shift;
-  my $item2 = shift;
-
-  #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  # check each dataset for duplicates 
-  #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  if ($item1->{buy} eq $item2->{buy}) {
-    _local_debug("[MAIN] : Found duplicate item (" . $item1->{buy} . "). Regenerating.");
-    return 1;
-  }
-
-  #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  # return data
-  #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  return 0;
-
-}
-
-################################################################################
-# _show_rules subroutine
-################################################################################
-sub _show_rules {
-
-  # read the rules from the rules inventory file
-  my $rules = ReadInventory::Read('rules');
-
-  # print out all the rules
-  foreach(split "\n", $rules) {
-    print "$_\n";
-  }
-
-  #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  # return
-  #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  return;
-
-}
-
-################################################################################
-# _show_help subroutine
-################################################################################
-sub _show_help {
-
-  # read the help from the help inventory file
-  my $help = ReadInventory::Read('help');
-
-  # print out all the help
-  foreach(split "\n", $help) {
-    print "$_\n";
-  }
-
-  #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  # return
-  #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  return;
-
 }
 
 ################################################################################
@@ -317,7 +118,7 @@ sub _local_debug {
 
   # only produce debug output if it is enabled for this module
   if ($DEBUG_STATE) {
-    Debug::Debug($msg);
+    Util::Debug::Debug($msg);
   }
 
   #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
